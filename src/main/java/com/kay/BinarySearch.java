f61eea822e1ad929f7d7ff3fb252c162769652cf
kaybee <passionno1@qq.com>
2021-12-23 13:48:56 +0800

    add BinarySearch

--- src/main/java/com/kay/BinarySearch.java
+++ src/main/java/com/kay/BinarySearch.java
@@ -2,6 +2,10 @@ package com.kay;
 
 public class BinarySearch {
 
+    private BinarySearch() {
+        //no-op
+    }
+
     public static int search(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
@@ -22,7 +26,7 @@ public class BinarySearch {
     public static int leftBound(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        while (left <= right) { // left + 1 = right
+        while (left <= right) { // when left > right, stop (left = right+1)
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 right = mid - 1; // move left
@@ -33,6 +37,7 @@ public class BinarySearch {
             }
         }
 
+        //when target > nums[nums.length-1], left stop at nums.length.
         if (left >= nums.length || nums[left] != target) {
             return -1;
         }
@@ -43,7 +48,7 @@ public class BinarySearch {
     public static int rightBound(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
-        while (left <= right) { // left + 1 = right
+        while (left <= right) { // when left > right, stop (left = right+1)
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 left = mid + 1; // move right
@@ -61,4 +66,16 @@ public class BinarySearch {
         return right;
     }
 
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 2, 2, 4};
+        Assert.isTrue(2 == search(nums, 2));
+        Assert.isTrue(1 == leftBound(nums, 2));
+        Assert.isTrue(3 == rightBound(nums, 2));
+
+        Assert.isTrue(-1 == leftBound(nums, 0));
+        Assert.isTrue(-1 == leftBound(nums, 5));
+
+        Assert.isTrue(-1 == rightBound(nums, 0));
+        Assert.isTrue(3 == rightBound(nums, 2));
+    }
 }
